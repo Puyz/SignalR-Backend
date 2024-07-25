3edd64a880938810b4bf1944a520ad41b4b948ab
“puyz” <puyzsoft@gmail.com>
2024-07-25 21:11:30 +0300

    first commit

---
+++ SignalR-Project/Controllers/MessagesController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+using SignalR_Project.Hubs;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace SignalR_Project.Controllers
+{
+    [Route("api/[controller]")]
+    public class MessagesController : Controller
+    {
+        private readonly IHubContext<MyHub> _hubContext;
+
+        public MessagesController(IHubContext<MyHub> hubContext)
+        {
+            _hubContext = hubContext;
+        }
+
+        [HttpPost("send")]
+        public async Task<IActionResult> SendMessageToClients(string message)
+        {
+            await _hubContext.Clients.All.SendAsync("receiveMessage", message);
+            return Ok();
+        }
+        
+        
+    }
+}
+
---
+++ SignalR-Project/Hubs/MyHub.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace SignalR_Project.Hubs;
+
+public class MyHub : Hub
+{
+    private static List<string> clients = new();
+
+	public async Task SendMessageAsync(string message)
+	{
+		await Clients.All.SendAsync("receiveMessage", message);
+	}
+
+    public override async Task OnConnectedAsync()
+    {
+        clients.Add(Context.ConnectionId);
+        await Clients.All.SendAsync("clients", clients);
+        await Clients.All.SendAsync("userConnected", Context.ConnectionId);
+    }
+    public override async Task OnDisconnectedAsync(Exception? exception)
+    {
+        clients.Remove(Context.ConnectionId);
+        await Clients.All.SendAsync("clients", clients);
+        await Clients.All.SendAsync("userDisconnected", Context.ConnectionId);
+    }
+}
+
+
